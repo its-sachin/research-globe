04647f9ebae253f5cf5f1117f52bf1cf0c3e629a
its-sachin <sachinsain28786@gmail.com>
2026-02-03 20:09:33 +0530

    Updates

--- src/data.ts
+++
@@ -1,4 +0,0 @@
-import type { ConferenceLocation } from './types';
-import publicationsJson from './content/generated/publications.json';
-
-export const conferenceData = publicationsJson as unknown as ConferenceLocation[];
